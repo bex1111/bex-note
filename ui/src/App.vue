42fc2d92a4d3fd8b4628383918f23db88403949a
Barna Barak <barna.barak@emarsys.com>
2025-10-09 19:24:58 +0200

    add markdown editor

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -3,23 +3,15 @@
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MarkdownEditor from './components/MarkdownEditor.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld: MarkdownEditor
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ ui/src/components/MarkdownEditor.vue
@@ -0,0 +1,46 @@
+
+<template>
+  <MdEditor v-model="text"
+            language="en-Us"
+            :toolbars="toolbar"
+  />
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import { MdEditor } from 'md-editor-v3';
+import 'md-editor-v3/lib/style.css';
+
+const text = ref('Hello Editor!');
+const toolbar=[
+  'revoke',
+  'next',
+  '-',
+  'bold',
+  'underline',
+  'italic',
+  '-',
+  'title',
+  'strikeThrough',
+  'sub',
+  'sup',
+  'quote',
+  'unorderedList',
+  'orderedList',
+  'task',
+  '-',
+  'codeRow',
+  'code',
+  'link',
+  'table',
+  '-',
+  'pageFullscreen',
+  'fullscreen',
+  'preview',
+  'previewOnly'
+];
+</script>
+
+<style scoped>
+
+</style>
